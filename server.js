042195fa8639ba81cbbc654731e31f417f3eee49
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-06-16 17:22:33 +0300

    json server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
 const PORT = process.env.PORT || 1451;
 
 app.use(express.static('projects/build'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './projects/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`)
